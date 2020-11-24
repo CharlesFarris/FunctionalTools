68c2037fe625c6c05c8e8690497cfe0e0d2652de
Charles Farris <charlesfarris71@gmail.com>
2020-11-23 23:55:30 -0700

    Add comments

--- src/FunctionalTools.Core/Maybe.cs
+++ src/FunctionalTools.Core/Maybe.cs
@@ -14,24 +14,46 @@ namespace FunctionalTools.Core
     public readonly struct Maybe<T> : IEquatable<Maybe<T>> where T : class
     {
         //--------------------------------------------------
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="value">The value being wrapped.</param>
         private Maybe([CanBeNull] T value)
         {
             this._value = value;
         }
 
+        /// <summary>
+        /// Returns true if the value is null.
+        /// </summary>
         public bool HasNoValue => this._value is null;
 
+        /// <summary>
+        /// Returns true if the value is not null.
+        /// </summary>
         public bool HasValue => !this.HasNoValue;
 
+        /// <summary>
+        /// Gets the value. If the value is null, an <see cref="InvalidOperationException"/>
+        /// is thrown.
+        /// </summary>
         [NotNull]
         // ReSharper disable once AssignNullToNotNullAttribute
         public T Value => this.HasNoValue
             ? throw new InvalidOperationException(Constants.ErrorMessages.MaybeHasNoValue)
             : this._value;
 
+        /// <summary>
+        /// Static empty instance.
+        /// </summary>
         public static readonly Maybe<T> Empty = new Maybe<T>(default);
 
         //--------------------------------------------------
+        /// <summary>
+        /// Implements the <see cref="IEquatable{T}"/> interface.
+        /// </summary>
+        /// <param name="other">Instance to be compared.</param>
+        /// <returns></returns>
         public bool Equals(Maybe<T> other)
         {
             if (this.HasNoValue && other.HasNoValue)
@@ -82,6 +104,12 @@ namespace FunctionalTools.Core
         }
 
         //--------------------------------------------------
+        /// <summary>
+        /// Factory method for creating a <see cref="Maybe{T}"/>
+        /// instance.
+        /// </summary>
+        /// <param name="value">Value being wrapped.</param>
+        /// <returns></returns>
         public static Maybe<T> From([CanBeNull] T value)
         {
             return new Maybe<T>(value);
